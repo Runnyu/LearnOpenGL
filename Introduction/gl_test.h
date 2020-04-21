0f1fdfec7c218bdc6d053898ab105a706ce70259
runnyu <runnyu@126.com>
2020-04-21 20:33:11 +0800

    chapter introduction

---
+++ Introduction/gl_test.h
@@ -0,0 +1,13 @@
+#ifndef GL_TEST_H
+#define GL_TEST_H
+
+int gl_test_window();
+int gl_test_triangle();
+int gl_test_ebo();
+int gl_test_shader();
+int gl_test_textures();
+int gl_test_transform();
+int gl_test_coordinate();
+int gl_test_camera();
+
+#endif
--- Introduction/main.cpp
+++ Introduction/main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include "gl_test.h"
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	// return gl_test_window();
+	// return gl_test_triangle();
+	// return gl_test_ebo();
+	// return gl_test_shader();
+	// return gl_test_textures();
+	// return gl_test_transform();
+	// return gl_test_coordinate();
+	return gl_test_camera();
 }
