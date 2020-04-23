f0446d8c4b12b0c23f7caebf4307757c2808460b
runnyu <runnyu@126.com>
2020-04-23 20:49:08 +0800

    base lighting

---
+++ Lighting/gl_test.h
@@ -0,0 +1,7 @@
+#ifndef GL_TEST_H
+#define GL_TEST_H
+
+int gl_test_color();
+int gl_test_base_lighting();
+
+#endif
---
+++ Lighting/main.cpp
@@ -0,0 +1,7 @@
+#include "gl_test.h"
+
+int main()
+{
+	// return gl_test_color();
+	return gl_test_base_lighting();
+}
